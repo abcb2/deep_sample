ca2e0a7b75c0a1932badc40672022b1727bec607
abcb2 <kouji.wtnb@gmail.com>
2017-09-22 13:21:44 +0900

    mnistのコードを写経

--- oreilly_deep_learning/dataset/mnist.py
+++ oreilly_deep_learning/dataset/mnist.py
@@ -88,3 +88,37 @@ def _change_one_hot_label(X):
         row[X[idx]] = 1
 
     return T
+
+
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    """
+    
+    :param normalize: 
+    :param flatten: 
+    :param one_hot_label: 
+    :return: 
+    """
+    if not os.path.exists(save_file):
+        init_mnist()
+
+    with open(save_file, 'rb') as f:
+        dataset = pickle.load(f)
+
+    if normalize:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].astype(np.float32)
+            dataset[key] /= 255.0
+
+    if one_hot_label:
+        dataset['train_label'] = _change_one_hot_label(dataset['train_label'])
+        dataset['test_label'] = _change_one_hot_label(dataset['test_label'])
+
+    if not flatten:
+        for key in ('train_img', 'test_img'):
+            dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
+
+    return (dataset['train_img'], dataset['train_label']), (dataset['test_img'], dataset['test_label'])
+
+
+if __name__ == '__main__':
+    init_mnist()
