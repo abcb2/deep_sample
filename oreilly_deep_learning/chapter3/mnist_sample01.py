010497a2be44de70ef3279024627b844cad6388c
abcb2 <kouji.wtnb@gmail.com>
2017-09-24 14:02:24 +0900

    add neural sample

--- oreilly_deep_learning/chapter3/mnist_sample01.py
+++ oreilly_deep_learning/chapter3/mnist_sample01.py
@@ -9,8 +9,11 @@ from dataset.mnist import load_mnist
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-print(x_train.shape)  # (60000, 784)
-print(t_train.shape)  # (60000, )
+print(x_train.shape)  # (60000, 784)   6万の画像(行）で、784次元(列)
+print(t_train.shape)  # (60000, )      6万のラベル(行）で、1次元(列)
 print(x_test.shape)  # (10000, 784)
 print(t_test.shape)  # (10000, )
 
+print(len(x_train[0]))
+print(t_train[0])
+print(len(t_train))
